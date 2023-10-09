2be5ccc66415ea7501c987a6730c98229f04ae48
Anse98 <lorenzo.anselmi98.com>
2023-10-09 15:48:57 +0200

    Codice ottimizzato e stampato output in html

--- app.js
+++ app.js
@@ -24,26 +24,31 @@ else {
 
 // DADI
 
-// - Generare numero da 1 a 6 per il computer e per l'utente
-let userNum = parseInt(Math.random() * (6 - 1 + 1) + 1);
-console.log("L'utente ha numero : " + userNum);
+// - Generare numero da 1 a 6 per l'utente 
+const userNum = parseInt(Math.random() * (6 - 1 + 1) + 1);
+const userNumber = document.querySelector(".user-number");
+userNumber.innerHTML =" "+ userNum;
 
-let pcNum = parseInt(Math.random() * (6 - 1 + 1) + 1);
-console.log("Il computer ha numero : " + pcNum);
+// - Generare numero da 1 a 6 per il computer
+const pcNum = parseInt(Math.random() * (6 - 1 + 1) + 1);
+const pcNumber = document.querySelector(".pc-number");
+pcNumber.innerHTML = " "+ pcNum;
 
-//   - A chi è uscito il numero piu alto è il vincitore
-//     - SE all'utente è uscito il numero piu alto stampare a schermo che ha  vinto
+//- A chi è uscito il numero piu alto è il vincitore
+//- SE all'utente è uscito il numero piu alto stampare a schermo che ha  vinto
+const win = document.querySelector(".win");
+
 if (userNum > pcNum) {
-  console.log("Ha vinto l'utente!");
+  win.innerHTML = "Hai vinto te!";
 }
 
 // SE è uscito un numero pari allora è pareggio
 else if (userNum === pcNum) {
-  console.log("Pareggio!");
+  win.innerHTML = "Hai pareggiato!";
 }
 
 //- ALTRIMENTI ha vinto il computer
  else {
-  console.log("Ha vinto il computer");
+  win.innerHTML = "Ha vinto il computer!";
 }
 
